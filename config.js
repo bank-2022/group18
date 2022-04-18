300a60dde218bfd05646c86e58529a74b603ab2f
nikkedoy98 <niklas.siltala@gmail.com>
2022-04-18 19:00:23 +0300

    :wrench: Päivitetty tietokannan osoite.

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "huitsinnevada.fi",
+    host: "old.huitsinnevada.fi",
     user: "u2_cXd0FxdOKm",
     password: "Ums5^.OMC@=!1WEaKkFbodqz",
     database: "s2_unnamedBankProject",
-  },
-  listPerPage: 10,
+  }
 };
 module.exports = config;
