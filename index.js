6d742c31b54865aa683dc981796caf514d9959af
nikkedoy98 <niklas.siltala@gmail.com>
2022-04-18 16:28:48 +0300

    :fire: Muutkin funktiot muokattu pienempään muotoon!

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const port = 3000;
 const bcrypt = require('bcryptjs');
 const db = require('./services/db');
 
-const bankapiRouter = require("./routes/clients");
-const bankapiRouterCards = require("./routes/cards");
-const bankapiRouterAccount = require("./routes/accounts");
-const bankapiRouterActions = require("./routes/transactions");
-
 var hash = bcrypt.hashSync("1221", bcrypt.genSaltSync(10));
 console.log("Has for PIN 1221 is: ", hash);
 
@@ -24,30 +19,71 @@ app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
 
+//Clients
 app.get('/clients', async function(req, res) {
   try {
     res.json(await db.query(`SELECT clientID, clientName, clientAddress, clientPhone FROM clients;`));
   } catch (err) {
-    console.error(`Error while getting Client data `, err.message);
+    console.error(`Error while getting Client data: `, err.message);
   }
 });
 
-app.use("/clients/:id", async function(req, res) {
+app.get("/clients/:id", async function(req, res) {
   try {
     res.json(await db.query(`SELECT clientName FROM clients WHERE clientID=${req.params.id};`));
   } catch (err) {
-    console.error(`Error while getting Client data `, err.message);
+    console.error(`Error while getting Client data: `, err.message);
   }
 });
 
-app.use("/cards", bankapiRouterCards);
-app.use("/cards/:id", bankapiRouterCards);
+//Cards
+app.get("/cards", async function(req, res) {
+  try {
+    res.json(await db.query(`SELECT cardID, cardPIN, clientID, accountID FROM cards;`));
+  } catch (err) {
+    console.error(`Error while getting Card data: `, err.message);
+  }
+});
 
-app.use("/accounts", bankapiRouterAccount);
-app.use("/accounts/:id", bankapiRouterAccount);
+app.get("/cards/:id", async function(req, res) {
+  try {
+    res.json(await db.query(`SELECT cardPin, accountID FROM cards WHERE cardID=${req.params.id};`));
+  } catch (err) {
+    console.error(`Error while getting Client data: `, err.message);
+  }
+});
 
-app.use("/transactions", bankapiRouterActions);
-app.use("/transactions/:id", bankapiRouterActions);
+//Accounts
+app.get("/accounts", async function(req, res) {
+  try {
+    res.json(await db.query(`SELECT accountID, balance FROM accounts;`));
+  } catch (err) {
+    console.error(`Error while getting Account data: `, err.message);
+  }
+});
+app.get("/accounts/:id", async function(req, res) {
+  try {
+    res.json(await db.query(`SELECT balance FROM accounts WHERE accountID=${req.params.id};`));
+  } catch (err) {
+    console.error(`Error while getting Account data: `, err.message);
+  }
+});
+
+//Transactions
+app.get("/transactions", async function(req, res) {
+  try {
+    res.json(await db.query(`SELECT transactionID, accountID, cardID, timestamp, action, amount FROM transactions;`));
+  } catch (err) {
+    console.error(`Error while getting Transaction data: `, err.message);
+  }
+});
+app.get("/transactions/:id", async function(req, res) {
+  try {
+    res.json(await db.query(`SELECT timestamp, amount FROM transactions WHERE accountID=${req.params.id};`));
+  } catch (err) {
+    console.error(`Error while getting Transaction data: `, err.message);
+  }
+});
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
